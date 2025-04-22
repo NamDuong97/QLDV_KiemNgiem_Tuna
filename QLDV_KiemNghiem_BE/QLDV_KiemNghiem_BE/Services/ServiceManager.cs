662481380e1c9d9d8ad8d88907b7a164401904ce
NamDuong97 <nakani3797@gmail.com>
2025-04-22 17:24:05 +0700

    Luu code ngay 22/4

---
+++ QLDV_KiemNghiem_BE/QLDV_KiemNghiem_BE/Services/ServiceManager.cs
@@ -0,0 +1,18 @@
+﻿using QLDV_KiemNghiem_BE.Interfaces;
+using QLDV_KiemNghiem_BE.Interfaces.ManagerInterface;
+
+namespace QLDV_KiemNghiem_BE.Services
+{
+    public class ServiceManager : IServiceManager
+    {
+        private readonly IRepositoryManager _repositoryManager;
+        private readonly Lazy<IPhieuDangKyService> _phieuDangKyService;
+        public ServiceManager(IRepositoryManager repositoryManager)
+        {
+            _repositoryManager = repositoryManager;
+            _phieuDangKyService = new Lazy<IPhieuDangKyService>(() => new PhieuDangKyService(repositoryManager));
+        }
+
+        public IPhieuDangKyService PhieuDangKy => _phieuDangKyService.Value;
+    }
+}
